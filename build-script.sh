b29f62f64095e6a21c1fe24b101cd3b5fe2568ed
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-04-02 17:15:04 +0300

    add build file

--- build-script.sh
+++ build-script.sh
@@ -3,6 +3,20 @@ git branch -D build-branch
 
 git checkout -b build-branch
 npm run build
+
+rm -rf public
+rm -rf src
+rm -rf package-lock.json
+rm -rf package.json
+rm -rf tsconfig.json
+rm -rf tsconfig.node.json
+rm -rf vite.config.ts
+rm -rf build-script.sh
+
+mv dist/index.html index.html
+mv dist/vite.svg vite.svg
+mv dist/assets assets
+
 echo "Project builded"
 git add -A
 git commit -m "new build"
