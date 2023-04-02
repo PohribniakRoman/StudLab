a6b4a4889d4420d9d09d1d3ea874facba94c3cc6
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-04-02 16:58:39 +0300

    add build file

--- build-script.sh
+++ build-script.sh
@@ -1,9 +1,11 @@
-echo "\nStart building project\n"
+echo "Start building project"
+git branch -D build-branch
+
 git checkout -b build-branch
 npm run build
-echo "\nProject builded\n"
+echo "Project builded"
 git add -A
 git commit -m "new build"
-git push
-echo "\nProject is on git,back to main branch\n"
-git checkout -b main
+git push -f origin build-branch
+echo "Project is on git,back to main branch"
+git checkout main
