2f39a221dbd5caa9d72e5de01bcccfcb507d1b20
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-04-21 20:09:20 +0300

    add animation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react"
 
 export const Form:React.FC<{children:ReactNode}> = ({children}:{children:ReactNode}) => {
     return <form className="auth__form">
+      <div className="auth__form--container">
         {children}
+      </div>
   </form>
 }
--- src/components/MailForm.tsx
+++ src/components/MailForm.tsx
@@ -4,8 +4,8 @@ import Button from "./ui-components/Button"
 export const MailForm:React.FC = ()=>{
     return <div className="auth__form--wrapper">
         <h1 className="auth__form--header"> <b>Для реєстрації введіть</b> <br /> Ваш студентський email</h1>
-        <div className="auth__form--input-container" style={{marginTop:"30px"}}>
-            <AuthFormInput style={{width:"94%"}} placeholder="Україні"/>
+        <div className="auth__form--input-container" style={{width:"65%"}}>
+            <AuthFormInput style={{width:"100%",marginBottom:"50px",marginTop:"50px"}} placeholder="Україні"/>
         </div>
         <div className="auth__form--submit-container" style={{justifyContent:"center"}}>
             <Button type="submit" title="Готово!"/>
