95a652fc93763be953ceb00071bdefc433eea69f
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-05-05 23:46:18 +0300

    Update calendar

--- src/components/widgets/Calendar.tsx
+++ src/components/widgets/Calendar.tsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const month = "січень, лютий, березень, квітень, травень, червень, липень, серпень, вересень, жовтень, листопад, грудень".split(", ");
 
 const days = ["Пн","Вт","Ср","Чт","Пт","Сб","Нд"];
 
+type CalendarInterface = {
+    label:string,
+    date:number,
+    current:boolean,
+    evented:boolean
+}
+
 export const Calendar:React.FC = () => {
 
-    const calendarData = [];
+    const defultState:CalendarInterface[] = [];
     for (let i = 0; i < days.length; i++) {
-        calendarData.push({
+        defultState.push({
             label:days[new Date().getDay()-1+i > 6?new Date().getDay()-8+i: new Date().getDay()-1+i],
             date:new Date().getDate()+i,
             current:i===0,
             evented:Math.random()*100>70
         })
-    }
+    } 
+
+    const [calendarData,setCalendar] = useState<CalendarInterface[]>(defultState);
+
+        
+    
     return <section className="calendar">
         <div className="calendar__arrow calendar__arrow--left"></div>
         <h3 className="calendar__month">{month[new Date().getMonth()]}</h3>
