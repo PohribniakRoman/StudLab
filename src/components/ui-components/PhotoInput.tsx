16696da6e44af88711fd25ceb3fe0b6f029c9791
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-05-08 14:32:09 +0300

    add profile widget

--- src/components/ui-components/PhotoInput.tsx
+++ src/components/ui-components/PhotoInput.tsx
@@ -1,11 +1,11 @@
 import {useState} from "react";
 import { Subtitle } from "./Subtitle";
 
-export const PhotoInput:React.FC<any> = ({formData,withLabel = true,defultAvatar = ""}) => {
+export const PhotoInput:React.FC<any> = ({formData,withLoad=true,withLabel = true,defultAvatar = ""}) => {
     const [avatar,setAvatar] = useState<string>(defultAvatar);
     return(
         <label className="auth__form--avatar-wrapper">
-            <input type="file" className="auth__form--file-input" accept=".jpg, .jpeg, .png" onChange={(event)=>{
+            {withLoad?<input type="file" className="auth__form--file-input" accept=".jpg, .jpeg, .png" onChange={(event)=>{
                 const file = event.target?.files?.item(0);
                 if(file!==null && file){
                     const reader = new FileReader();
@@ -15,7 +15,7 @@ export const PhotoInput:React.FC<any> = ({formData,withLabel = true,defultAvatar
                     }
                     reader.readAsDataURL(file);
                 }
-            }}/>
+            }}/>:""}
             <div className="auth__form--avatar" style={{backgroundImage:`url("${avatar}")`}}></div>
             {withLabel?<Subtitle className="auth__form--input-title" title="Фото"/>:""}
         </label>
--- src/components/widgets/Profile.tsx
+++ src/components/widgets/Profile.tsx
@@ -5,7 +5,22 @@ export const Profie:React.FC = () =>{
     return <section className="profile">
         <h1 className="profile__title">Профіль</h1>
         <div className="profile__container">
-            <PhotoInput withLabel={false} defultAvatar="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/1200px-President_Barack_Obama.jpg"/>
+            <PhotoInput withLabel={false} withLoad={false} defultAvatar="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/1200px-President_Barack_Obama.jpg"/>
+            <div className="profile__data">
+                <h3 className="profile__name" data-course={"1 курс"}>Богдан Дубчак</h3>
+                <p className="profile__entry">
+                    <b>Місто:</b>
+                    Київ
+                </p>
+                <p className="profile__entry">
+                    <b>Факультет:</b>
+                    Прикладна математика
+                </p>
+                <p className="profile__entry">
+                    <b>Місце навчання:</b>
+                    <i>Київський національний університет</i>
+                </p>
+            </div>
         </div>
     </section>
 }
