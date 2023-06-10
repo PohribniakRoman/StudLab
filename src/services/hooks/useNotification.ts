fcee245a44cee9e5b40a0e3efcda7fc41c5d87fa
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-06-10 13:28:58 +0300

    add fix

--- src/services/hooks/useNotification.ts
+++ src/services/hooks/useNotification.ts
@@ -1,14 +1,13 @@
 import { useDispatch, useSelector } from "react-redux"
 import { Notification } from "../reducers/notificationReducer";
-import { Store } from "../reducers/combinedReducer";
 import { v4 } from 'uuid';
 
 export const useNotification = () =>{
-    const notifications = useSelector((state:Store)=>state.notifications);
+    const notifications = useSelector((state:any)=>state.notifications);
     const dispatch = useDispatch();
     const createNotification = (message:string,type:Notification["type"]) => {
         const id = v4();
-        if(notifications.filter(e=>e.message === message).length === 0){
+        if(notifications.filter((e:any)=>e.message === message).length === 0){
             dispatch({type:"ADD_NOTIFICATION",payload:{
                 message,
                 type,
